09b4b86835d464786d1a371d220666f15954a3a3
Amr Noman <amrnn90@gmail.com>
2022-07-11 18:34:10 +0300

    style: use explicit imports in auth plugin

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,3 +1,5 @@
+import { useUser, fetchCurrentUser } from "~/composables/useAuth";
+
 export default defineNuxtPlugin(async () => {
   const user = useUser();
   user.value = await fetchCurrentUser();
