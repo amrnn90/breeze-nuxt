cdf79fbbce7a21966bd5a98b2c48dcaff2164fc1
Amr Noman <amrnn90@gmail.com>
2022-08-13 00:19:06 +0300

    feat: use bunny fonts

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 <template>
   <Head>
     <Link
-      href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+      href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"
       rel="stylesheet"
     />
   </Head>
