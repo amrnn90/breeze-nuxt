fba8c8fa7f230e68f211db142d850124dde20def
Amr Noman <amrnn90@gmail.com>
2022-09-06 08:45:29 +0300

    fix: remove extra div wrapper for validation errors

--- components/Input.vue
+++ components/Input.vue
@@ -17,23 +17,20 @@ const renderedErrors = computed(() =>
   Array.isArray(props.errors)
     ? props.errors
     : props.errors
-    ? [props.errors]
-    : []
+      ? [props.errors]
+      : []
 );
 </script>
 
 <template>
   <div>
-    <input
-      :value="modelValue"
-      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    <input :value="modelValue" @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
       class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-      v-bind="$attrs"
-    />
-    <div v-if="renderedErrors?.length > 0">
-      <ul className="mt-3 text-sm text-red-600">
-        <li v-for="error in renderedErrors" :key="error">{{ error }}</li>
-      </ul>
-    </div>
+      v-bind="$attrs" />
+
+    <!-- Validation Errors -->
+    <ul v-if="renderedErrors?.length > 0" class="mt-3 text-sm text-red-600">
+      <li v-for="error in renderedErrors" :key="error">{{ error }}</li>
+    </ul>
   </div>
 </template>
