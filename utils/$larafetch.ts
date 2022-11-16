9baed89dcb9ac91c35bc268abb2d7e3043a3ec87
Amr Noman <amrnn90@gmail.com>
2022-11-16 21:35:03 +0300

    refactor

--- utils/$larafetch.ts
+++ utils/$larafetch.ts
@@ -68,18 +68,20 @@ export async function $larafetch<T, R extends ResponseType = "json">(
     // when any of the following redirects occur and the final throw is not caught then nuxt SSR will log the following error:
     // [unhandledRejection] Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
 
+    const status = error.response?.status ?? -1;
+
     if (
       redirectIfNotAuthenticated &&
-      [401, 419].includes(error.response?.status ?? -1)
+      [401, 419].includes(status)
     ) {
       await router.push('/login')
     }
 
-    if (redirectIfNotVerified && [409].includes(error.response?.status ?? -1)) {
+    if (redirectIfNotVerified && [409].includes(status)) {
       await router.push('/verify-email')
     }
 
-    if ([500].includes(error.response?.status ?? -1)) {
+    if ([500].includes(status)) {
       console.error("[Laravel Error]", error.data?.message, error.data);
     }
 
