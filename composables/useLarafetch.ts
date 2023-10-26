fdb164b1e4108d2a316f10489eebbf6f6a589f5c
Amr Noman <amrnn90@gmail.com>
2023-10-26 03:28:29 +0300

    feat: `useLarafetch` composable

---
+++ composables/useLarafetch.ts
@@ -0,0 +1,30 @@
+import type { UseFetchOptions } from "nuxt/app";
+
+export function useLarafetch<T>(
+  url: string | (() => string),
+  options: UseFetchOptions<T> = {}
+) {
+  return useFetch(url, {
+    $fetch: $larafetch,
+    async onResponseError({ response }) {
+      const status = response.status;
+      if ([500].includes(status)) {
+        console.error(
+          "[Laravel Error]",
+          response.statusText,
+          ":",
+          response._data
+        );
+      }
+
+      if ([401, 419].includes(status)) {
+        navigateTo("/login");
+      }
+
+      if ([409].includes(status)) {
+        navigateTo("/verify-email");
+      }
+    },
+    ...options,
+  });
+}
