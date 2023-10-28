ef08f78d121b6e971c2a298aaad88447c387e6c4
Amr Noman <amrnn90@gmail.com>
2023-10-28 18:43:07 +0300

    fix: error message format

--- composables/useLarafetch.ts
+++ composables/useLarafetch.ts
@@ -12,7 +12,6 @@ export function useLarafetch<T>(
         console.error(
           "[Laravel Error]",
           response.statusText,
-          ":",
           response._data
         );
       }
